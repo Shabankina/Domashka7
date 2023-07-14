808f0e3386980164a37c37cc7919ab8ff3d6c650
«Marina <marinka377@gmail.com>
2023-07-14 19:27:56 +0300

    Не получается заполнить двумерный массив

---
+++ Zadach47/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+
+string[,] matr = new string[3,4];
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(-100,100);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]}");
+        }
+    }
+}    
+FillArray(matr);
+PrintArray(matr);
---
+++ Zadach50/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+string[,] matr = new string[3,4];
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(-100,100);
+        }
+    }
+}
+
+void PrintArray(int[,] matr);
+int[,] matr = new int[3,4];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]}");
+        }
+        Console.Writeline();
+    }
+FillArray(matr);
+PrintArray(matr);
+
